9f39ba8eb81bb1e2554e6c77f6d70831d5a7f810
zio <zio@hypercube.games>
2025-09-05 00:16:31 +0900

    Solved Backjun Problem18108

---
+++ Problems/SolvedAc/Problem18108.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 18108번 문제 - 1998년생인 내가 태국에서는 2541년생?!
+    /// </summary>
+    internal class Problem18108
+    {
+        public static void Run()
+        {
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(input - 543);
+        }
+    }
+}
