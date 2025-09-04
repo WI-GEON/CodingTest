5763f6333169f714d54763077a6d3b1f709020c8
zio <zio@hypercube.games>
2025-09-05 00:16:12 +0900

    Solved Backjun Problem1330

---
+++ Problems/SolvedAc/Problem1330.cs
@@ -0,0 +1,21 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 1330번 문제 - 두 수 비교하기
+    /// </summary>
+    internal class Problem1330
+    {
+        public static void Run()
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            if (A > B)      Console.WriteLine(">");
+            else if (A < B) Console.WriteLine("<");
+            else            Console.WriteLine("==");
+
+            // Console.WriteLine(A == B ? "==" : (A > B ? ">" : "<"));
+        }
+    }
+}
