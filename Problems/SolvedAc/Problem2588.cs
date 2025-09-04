44c59a5fb92421d150af67b2d5f351103a4f2086
zio <zio@hypercube.games>
2025-09-05 00:16:59 +0900

    Solved Backjun Problem2588

---
+++ Problems/SolvedAc/Problem2588.cs
@@ -0,0 +1,21 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// soled.ac 2588번 문제 - 곱셈
+    /// </summary>
+    internal class Problem2588
+    {
+        public static void Run()
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int first = B % 10;
+            int second = (B / 10) % 10;
+            int third = B / 100;
+            Console.WriteLine(A * first);
+            Console.WriteLine(A * second);
+            Console.WriteLine(A * third);
+            Console.WriteLine(A * B);
+        }
+    }
+}
