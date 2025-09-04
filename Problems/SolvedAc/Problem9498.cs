b6acf202830be380851bcfcecc86f5798ebedd37
zio <zio@hypercube.games>
2025-09-05 00:19:58 +0900

    Solved Backjun Problem9498

---
+++ Problems/SolvedAc/Problem9498.cs
@@ -0,0 +1,18 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 9498번 문제 - 시험 성적
+    /// </summary>
+    internal class Problem9498
+    {
+        public static void Run()
+        {
+            var score = int.Parse(Console.ReadLine());
+            if (score >= 90)       Console.WriteLine("A");
+            else if (score >= 80)  Console.WriteLine("B");
+            else if (score >= 70)  Console.WriteLine("C");
+            else if (score >= 60)  Console.WriteLine("D");
+            else                   Console.WriteLine("F");
+        }
+    }
+}
