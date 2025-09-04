e0a2d655a8527179b01c27ba4b6e25cb1ac0d710
zio <zio@hypercube.games>
2025-09-05 00:26:23 +0900

    Modify Builder.cs

--- Builder.cs
+++ Builder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodingTest.Problems
+﻿namespace CodingTest.Problems
 {
     internal class Builder
     {
