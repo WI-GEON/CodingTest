36bdd4f2cb54f6704b8a1f8f14aa6bf3d844ec6e
zio <zio@hypercube.games>
2025-09-05 00:14:56 +0900

    Solved Backjun Problem10430

---
+++ Problems/SolvedAc/Problem10430.cs
@@ -0,0 +1,20 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 10430번 문제 - 나머지
+    /// </summary>
+    internal class Problem10430
+    {
+        public static void Run()
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int C = int.Parse(input[2]);
+            Console.WriteLine((A + B) % C);
+            Console.WriteLine(((A % C) + (B % C)) % C);
+            Console.WriteLine((A * B) % C);
+            Console.WriteLine(((A % C) * (B % C)) % C);
+        }
+    }
+}
