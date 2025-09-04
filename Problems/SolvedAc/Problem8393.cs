b992ea00b7a33fc9b5ee2ce038968a3f232b195a
zio <zio@hypercube.games>
2025-09-05 00:19:49 +0900

    Solved Backjun Problem8393

---
+++ Problems/SolvedAc/Problem8393.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 8393번 문제 - 합
+    /// </summary>
+    internal class Problem8393
+    {
+        public static void Run()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n * (n + 1) / 2); // 등차수열의 합 공식~
+        }
+    }
+}
