9a5f4f363b2580b2c8b1af4168710067e0a41d33
zio <zio@hypercube.games>
2025-09-05 00:16:02 +0900

    Solved Backjun Problem11382

---
+++ Problems/SolvedAc/Problem11382.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 11382번 문제 - 꼬마 정민
+    /// </summary>
+    internal class Problem11382
+    {
+        public static void Run()
+        {
+            string[] input = Console.ReadLine().Split();
+            long A = long.Parse(input[0]);
+            long B = long.Parse(input[1]);
+            long C = long.Parse(input[2]);
+            Console.WriteLine(A + B + C);
+        }
+    }
+}
