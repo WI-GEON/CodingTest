f6aeb7b10bbaf0fdca9a9224c21c11cc730c8746
zio <zio@hypercube.games>
2025-09-05 00:12:34 +0900

    Solved Backjun Problem1008
    백준, Solved.ac 1008번 문제 해결

---
+++ Problems/SolvedAc/Problem1008.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    internal class Problem1008
+    {
+        public static void Run()
+        {
+            string[] input = Console.ReadLine().Split();
+            double a = double.Parse(input[0]);
+            double b = double.Parse(input[1]);
+
+            Console.WriteLine((a / b).ToString("R"));
+        }
+    }
+}
