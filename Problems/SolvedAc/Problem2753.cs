27c6fb0c94a9f4af0ea44545d59ae1be044f4b8e
zio <zio@hypercube.games>
2025-09-05 00:19:33 +0900

    Solved Backjun Problem2753

---
+++ Problems/SolvedAc/Problem2753.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 2753번 문제 - 윤년
+    /// </summary>
+    internal class Problem2753
+    {
+        public static void Run()
+        {
+            var year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(0);
+
+            // Console.WriteLine((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? 1 : 0);
+        }
+    }
+}
