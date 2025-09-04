e1b39b2e1f7822d2ee0356a6ddc206f93e9e83f6
zio <zio@hypercube.games>
2025-09-05 00:15:45 +0900

    Solved Backjun Problem10950

---
+++ Problems/SolvedAc/Problem10950.cs
@@ -0,0 +1,22 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 10950번 문제 - A+B - 3
+    /// </summary>
+    internal class Problem10950
+    {
+        public static void Run()
+        {
+            var T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var A = int.Parse(input[0]);
+                var B = int.Parse(input[1]);
+
+                Console.WriteLine(A + B);
+            }
+        }
+    }
+}
