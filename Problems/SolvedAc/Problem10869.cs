cb8a0630a7231de52eb5ea8b150b02025121605c
zio <zio@hypercube.games>
2025-09-05 00:14:46 +0900

    Solved Backjun Problem10869
    백준, Solved.ac 10869 문제 해결

---
+++ Problems/SolvedAc/Problem10869.cs
@@ -0,0 +1,20 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 10869번 문제 - 사칙연산
+    /// </summary>
+    internal class Problem10869
+    {
+        public static void Run()
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
