15e81b4ec1d7a372c6943328c9725147a7749d70
zio <zio@hypercube.games>
2025-09-05 00:15:07 +0900

    Solved Backjun Problem10950

---
+++ Problems/SolvedAc/Problem10926.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 10926번 문제 - ??!
+    /// </summary>
+    internal class Problem10926
+    {
+        public static void Run()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input + "??!");
+        }
+    }
+}
