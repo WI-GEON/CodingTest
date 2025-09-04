eabf845aef151117d55b73aa7657289bebeb28d6
zio <zio@hypercube.games>
2025-09-05 00:19:25 +0900

    Solved Backjun Problem2739

---
+++ Problems/SolvedAc/Problem2739.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingTest.Problems.SolvedAc
+{
+    /// <summary>
+    /// solved.ac 2739번 문제 - 구구단
+    /// </summary>
+    internal class Problem2739
+    {
+        public static void Run()
+        {
+            var inputNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{inputNum} * {i} = {inputNum * i}");
+            }
+
+            // Enumerable.Range(1, 9).ToList().ForEach(i => Console.WriteLine($"{n} * {i} = {n * i}"));
+        }
+    }
+}
