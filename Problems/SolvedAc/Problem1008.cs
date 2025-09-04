dbe35954bbb26875abff1d4b88754e7f82e85785
WIGEON <wegeon.dev@gmail.com>
2025-09-05 00:30:36 +0900

    Modify Problem1008.cs

--- Problems/SolvedAc/Problem1008.cs
+++ Problems/SolvedAc/Problem1008.cs
@@ -1,5 +1,8 @@
 ﻿namespace CodingTest.Problems.SolvedAc
 {
+    /// <summary>
+    /// solved.ac 1008번 문제 - A/B
+    /// </summary>
     internal class Problem1008
     {
         public static void Run()
@@ -8,7 +11,7 @@
             double a = double.Parse(input[0]);
             double b = double.Parse(input[1]);
 
-            Console.WriteLine((a / b).ToString("R"));
+            Console.WriteLine((a / b).ToString("R"));\
         }
     }
 }
